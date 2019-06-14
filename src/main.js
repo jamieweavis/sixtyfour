4065965cc1c549f90ceefa5d7c7a1780403fffaf
Jamie Weavis <jamie.weavis@gmail.com>
2019-06-14 14:33:00 +0100

    Add build

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ const path = require('path');
 const { menubar } = require('menubar');
 
 const mb = menubar({
-  tooltip: 'sixtyfour',
+  tooltip: 'sixtyfour - base64 encoder/decoder',
   icon: path.join(__dirname, 'iconTemplate@2x.png'),
   index: path.join('file://', __dirname, 'index.html'),
+  preloadWindow: true,
 });
