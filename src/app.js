948491507a4aa22e716a4197f1952f5e032322c7
Jamie Weavis <jamie.weavis@gmail.com>
2019-06-18 18:01:46 +0100

    Move renderer files into window dir

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const { menubar } = require('menubar');
 
-const mb = menubar({
+menubar({
   tooltip: 'sixtyfour - base64 encoder/decoder',
   icon: path.join(__dirname, 'iconTemplate@2x.png'),
-  index: path.join('file://', __dirname, 'index.html'),
+  index: path.join('file://', __dirname, 'window', 'index.html'),
   preloadWindow: true,
 });
--- src/window/script.js
+++ src/window/script.js
@@ -3,9 +3,15 @@ const decodeTextArea = document.querySelector('#decode');
 const newline = /\r\n|\n|\r/;
 
 encodeTextArea.addEventListener('keyup', ({ target: { value } }) => {
-  decodeTextArea.value = value.split(newline).map(v => btoa(v)).join('\n');
+  decodeTextArea.value = value
+    .split(newline)
+    .map(v => btoa(v))
+    .join('\n');
 });
 
 decodeTextArea.addEventListener('keyup', ({ target: { value } }) => {
-  encodeTextArea.value = value.split(newline).map(v => atob(v)).join('\n');
+  encodeTextArea.value = value
+    .split(newline)
+    .map(v => atob(v))
+    .join('\n');
 });
